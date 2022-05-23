2fe5f0bda2c88365602e5b30aec3b3c832120161
Bouchbout-sami <ks_bouchbout@esi.dz>
2022-05-23 22:34:43 +0100

    historic

--- index.js
+++ index.js
@@ -11,12 +11,30 @@ app.on("ready", () => {
     height: 680,
     minWidth: 940,
     minHeight: 560,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
+  splash = new BrowserWindow({
+    width: 1200,
+    height: 680,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+  });
+  splash.loadFile("./src/splashScreen/SplashScreen.html"); //chargeement du fichier html
+  setTimeout(function () {
+    splash.destroy();
+    mainWindow.loadFile("./src/Home-page/home.html");
+    mainWindow.show();
+  }, 8000);
   mainWindow.loadURL(`file://${__dirname}/src/Home-page/home.html`);
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
 
 function quitApplication() {
